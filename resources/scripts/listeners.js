197baba805fb172956bc36e2da2b920a3985bfe6
TheSlip <daverutsch@gmail.com>
2015-08-09 18:34:27 -0500

    Created interval to update output every one second

--- resources/scripts/listeners.js
+++ resources/scripts/listeners.js
@@ -2,4 +2,5 @@ document.addEventListener('DOMContentLoaded', installListeners);
 
 function installListeners () {
   document.getElementById('submit-btn').addEventListener('click', app.main)
+  setInterval(app.main, 1000);
 };
