bd53aa6571993202a56dc024ae7e8b933470dd4d
TheSlip <daverutsch@gmail.com>
2015-08-09 18:17:54 -0500

    Moved dateDifference constructor and prototypes into it's own file

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -23,28 +23,6 @@ app.getDateFromInput = function getDateFromInput() {
   return dateEnteredByUser;
 };
 
-app.dateDifference = function dateDifference(name, date1, date2) {
-  this.name = name;
-  this.date1 = date1;
-  this.date2 = date2;
-  this.time = this.addDifference(this.date1, this.date2, name);
-};
-
-app.dateDifference.prototype.addDifference = function addDifference(date1, date2, unitOfTime) {
-  var difference = date1.diff(date2, unitOfTime);
-  date2.add(moment.duration(difference, unitOfTime));
-
-  return difference;
-};
-
-app.dateDifference.prototype.getName = function getName() {
-  return this.name;
-}
-
-app.dateDifference.prototype.getTime = function getName() {
-  return this.time;
-}
-
 app.getDifferenceBetweenDates = function getDifferenceBetweenDates() {
   var userDate = app.getDateFromInput();
   var currentTime = moment();
