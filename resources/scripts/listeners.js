dc96853f03c8eda42c452774c3e85318d9b4c7bf
TheSlip <daverutsch@gmail.com>
2015-08-10 00:00:05 -0500

    Disabled interval since seconds aren't being used

--- resources/scripts/listeners.js
+++ resources/scripts/listeners.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', installListeners);
 
 function installListeners () {
-  document.getElementById('submit-btn').addEventListener('click', installInterval);
-};
-
-function installInterval () {
-  var userInput = app.getTextFromElement('date');
-
-  if (userInput) {
-    setInterval(app.main, 1000);
+document.getElementById('date').onkeypress = function(e) {
+    if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+      console.log('enter pressed');
+      app.main();
+      // installInterval();
+      return false;
+    }
   }
-};
+}
+
+//Only needed when seconds are enabled
+// function installInterval () {
+//   var userInput = app.getTextFromElement('date');
+//
+//   if (userInput) {
+//     setInterval(app.main, 1000);
+//   }
+// };
