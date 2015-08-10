ef70354d9ab739f0d2157c8b6d412e50ce3f2ca7
TheSlip <daverutsch@gmail.com>
2015-08-09 21:05:01 -0500

    Changed server config to match new file structure

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'timesince')));
 app.use('/resources', express.static(__dirname + '/resources'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/components', express.static(__dirname + '/components'));
 
 app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname + '/index.html'))
