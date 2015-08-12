d3f93fc544a36edd6150c5750543597de8fb1bf8
TheSlip <daverutsch@gmail.com>
2015-08-12 00:27:48 -0500

    Added logging. Added jade module and setup server to use jade templating

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('view engine', 'jade');
+
 app.use(express.static(path.join(__dirname, 'timesince')));
 app.use('/resources', express.static(__dirname + '/resources'));
 app.use('/components', express.static(__dirname + '/components'));
 
 app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname + '/index.html'))
+  res.render('index', { title: 'timesince'});
 });
 
-app.listen(8080);
+app.listen(3000, function() {
+  console.log("I'm sorry, Dave. I'm afraid I can't do that");
+});
