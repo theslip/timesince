1784f00458730dc0220a9361a001273ceb1de94b
David <daverutsch@gmail.com>
2015-08-15 14:55:21 -0500

    ... uncommented

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000);
 
 // app.use(enableCors);
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'timesince')));
 app.use('/public', express.static(__dirname + '/public'));
