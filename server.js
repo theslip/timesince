2fb3904f7b2e1a0ab7ba4df14b079873057f6cf5
David <daverutsch@gmail.com>
2015-08-16 00:08:26 -0500

    Added subdomain module back

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var differenceController  = require('./app/controllers/differenceController'),
     testController        = require('./app/controllers/testController'),
+    subdomain             = require('express-subdomain'),
     bodyParser            = require('body-parser'),
     express               = require('express'),
     path                  = require('path'),
     app                   = express(),
     router                = express.Router();
 
-app.set('port', process.env.PORT || 3000);
+var port = app.set('port', process.env.PORT || 3000);
 
 // var enableCors = require('./app/config/cors');
 
@@ -14,26 +15,26 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'timesince')));
+// app.use(express.static(path.join(__dirname, 'timesince')));
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-router.get('/', function(req, res){
-  res.sendFile(__dirname + "/app/views/index.html");
+router.get('/', function(req, res) {
+  // res.sendFile(__dirname + "/app/views/index.html");
+  res.send('Q!');
 });
 
-router.post('/difference', differenceController.getInputFromUser);
-router.get('/test', testController.getTestData);
+require('./app/config/routes')(app, differenceController, testController);
 
-app.use('/', router);
+app.use(subdomain('timesince', router));
 
 app.use('*', function(req, res) {
+  res.status(404);
   res.sendFile(__dirname + "/app/views/404.html");
 });
 
+app.use(subdomain('timesince', router));
 
-// require('./app/config/routes')(app, differenceController, testController);
-// app.use(subdomain('dev', router));
 app.listen(app.get('port'), function() {
   console.log("I'm sorry, Dave. I'm afraid I can't do that.");
 });
