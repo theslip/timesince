b5d6f3414bf876d9edcca72c2b2f1b61831ae2b5
David <daverutsch@gmail.com>
2015-08-15 22:34:04 -0500

    Cleaned up code

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var differenceController  = require('./app/controllers/differenceController'),
     app                   = express(),
     router                = express.Router();
 
-app.set('port', process.env.PORT || 3000);
+var port = app.set('port', process.env.PORT || 3000);
 
 // var enableCors = require('./app/config/cors');
 
@@ -18,22 +18,20 @@ app.use(express.static(path.join(__dirname, 'timesince')));
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-router.get('/', function(req, res){
+router.get('/*', function(req, res){
   res.sendFile(__dirname + "/app/views/index.html");
 });
 
 router.post('/difference', differenceController.getInputFromUser);
 router.get('/test', testController.getTestData);
 
-app.use('/', router);
-
 app.use('*', function(req, res) {
+  res.status(404);
   res.sendFile(__dirname + "/app/views/404.html");
 });
 
+require('./app/config/routes')(app, differenceController, testController);
 
-// require('./app/config/routes')(app, differenceController, testController);
-// app.use(subdomain('dev', router));
 app.listen(app.get('port'), function() {
   console.log("I'm sorry, Dave. I'm afraid I can't do that.");
 });
