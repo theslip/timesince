870c6629624f0ffb6b9f66c9790dc4c0637e6d4b
David <daverutsch@gmail.com>
2015-08-19 22:28:02 -0500

    Added all application middleware bindings to the serverconfig module. Passed in this.app to avoid redundancy

--- app/config/serverConfig.js
+++ app/config/serverConfig.js
@@ -1,6 +1,9 @@
-var bodyParser = require('body-parser');
+var express = require('express'),
+    bodyParser = require('body-parser');
 
 module.exports = function serverConfig(app) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use('/public', express.static('./public'));
+  app.use('/bower_components', express.static('./bower_components'));
 }
