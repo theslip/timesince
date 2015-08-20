b6b44900dc768bdbf34fa15200bf23b6a1b86e6d
David <daverutsch@gmail.com>
2015-08-19 21:32:08 -0500

    Created a server class to export as a module. Passed port into constructor rather than the start function. Modularized server configuration options

--- app/server.js
+++ app/server.js
@@ -1,16 +1,18 @@
 var differenceController  = require('./controllers/differenceController'),
-    bodyParser            = require('body-parser'),
     express               = require('express'),
-    path                  = require('path'),
+    path                  = require('path');
     app                   = express(),
     router                = express.Router();
 
-var server = function server() {
+var server = function server(port) {
+  this.port = port;
+  this.initializeConfig();
+};
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+server.prototype.initializeConfig = function initializeConfig() {
 
-  app.use(express.static(path.join(__dirname, 'timesince')));
+  var serverConfig = require('./config/serverConfig')(app);
+
   app.use('/public', express.static('./public'));
   app.use('/bower_components', express.static('./bower_components'));
 
@@ -26,16 +28,16 @@ var server = function server() {
     res.status(404);
     res.sendFile(__dirname + '/views/404.html');
   });
+};
 
-  server.prototype.destroy = function destroy() {
-    app.close();
-  };
+server.prototype.destroy = function destroy() {
+  this.serverInstance.close();
+};
 
-  server.prototype.start = function start(port) {
-    app.listen(port, function() {
-      console.log("I'm sorry, Dave. I'm afraid I can't do that.");
-    });
-  };
+server.prototype.start = function start() {
+  this.serverInstance = app.listen(this.port, function() {
+    console.log("I'm sorry, Dave. I'm afraid I can't do that.");
+  });
 };
 
 module.exports = server;
