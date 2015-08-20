d751c024973bd694981a122b605debdddb0ef015
David <daverutsch@gmail.com>
2015-08-20 00:17:38 -0500

    Returned 200 response to get test passing

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,6 +3,7 @@ differenceController  = require('../controllers/differenceController');
 module.exports = function(router) {
   router.post('/difference', differenceController.getInputFromUser);
   router.get('/', function(req, res){
+    res.status(200);
     res.sendFile('index.html', {"root": 'app/views'});
   });
   router.use(function(req, res) {
