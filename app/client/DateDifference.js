077a38c139748fda988fa20c141937816a130aaf
David Rutsch <daverutsch@gmail.com>
2016-03-04 22:30:20 -0600

    Created client entry point

---
+++ app/client/DateDifference.js
@@ -0,0 +1,16 @@
+// import moment from 'moment'
+//
+// class DateDifference {
+//   constructor (unit, lhs, rhs) {
+//     this.unit = unit
+//     this.lhs = lhs
+//     this.rhs = rhs
+//   }
+//   getDifference () {
+//     const difference = this.lhs.diff(this.rhs, this.unit)
+//     this.rhs.add(moment.duration(difference, this.unit))
+//     return difference
+//   }
+// }
+//
+// export default DateDifference
--- app/client/listeners.js
+++ app/client/listeners.js
@@ -1,30 +1,30 @@
 'use strict'
 
-var main = require('./clientApp')
+// var main = require('./clientApp')
 
 document.addEventListener('DOMContentLoaded', function domOnLoad () {
   document.removeEventListener('DOMContentLoaded', domOnLoad, false)
-  onDomReady()
+  // onDomReady()
   fadeWebPageIn()
 }, false)
 
-var onDomReady = function onDomReady () {
-  var differenceInputField = document.getElementById('difference')
-  var dateInput = document.getElementById('date')
+// var onDomReady = function onDomReady () {
+//   var differenceInputField = document.getElementById('difference')
+//   var dateInput = document.getElementById('date')
+//
+//   dateInput.focus()
+//   differenceInputField.disabled = true
+//   watchForEnterKeyOnDateInput(dateInput)
+// }
 
-  dateInput.focus()
-  differenceInputField.disabled = true
-  watchForEnterKeyOnDateInput(dateInput)
-}
-
-var watchForEnterKeyOnDateInput = function watchForEnterKeyOnDateInput (userInput) {
-  userInput.onkeypress = function (event) {
-    var keyCode = event.keyCode || event.which
-    if (keyCode === 13) {
-      main()
-    }
-  }
-}
+// var watchForEnterKeyOnDateInput = function watchForEnterKeyOnDateInput (userInput) {
+//   userInput.onkeypress = function (event) {
+//     var keyCode = event.keyCode || event.which
+//     if (keyCode === 13) {
+//       main()
+//     }
+//   }
+// }
 
 var fadeWebPageIn = function fadeWebPageIn () {
   document.body.classList.add('visible')
