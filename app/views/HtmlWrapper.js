9132df0908d57ee887c00756a57dce228589a5e5
David Rutsch <daverutsch@gmail.com>
2016-03-07 15:50:03 -0600

    Used absolute urls to prevent links from breaking

--- app/views/HtmlWrapper.js
+++ app/views/HtmlWrapper.js
@@ -19,12 +19,12 @@ class HtmlWrapper extends React.Component {
         <head>
           <title>{title}</title>
           <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' />
-          <link rel='stylesheet' type='text/css' href='public/styles/site.css' />
-          <link rel='icon' href='public/favicon.ico' />
+          <link rel='stylesheet' type='text/css' href='/public/styles/site.css' />
+          <link rel='icon' href='/public/favicon.ico' />
         </head>
         <body>
           <div id='content' dangerouslySetInnerHTML={{__html: content}}></div>
-          <script type='text/javascript' src='public/scripts/bundle.js'></script>
+          <script type='text/javascript' src='/public/scripts/bundle.js'></script>
           <script dangerouslySetInnerHTML={{__html: `window.__initialState__=${serialize(store.getState())};`}} charSet='UTF-8'/>
         </body>
       </html>
