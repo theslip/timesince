c00f67c34a5bbfbc7c03fc2a33638007818e2d7f
David Rutsch <daverutsch@gmail.com>
2016-03-19 00:26:19 -0500

    Added absolute value. Cleaned up code

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -5,21 +5,20 @@ const singularUnits = ['year', 'month', 'week', 'day', 'hour', 'minute']
 
 const getUnit = (diff, unit, index) => diff === 1 ? singularUnits[index] : unit
 const getDiff = (lhs, rhs, unit) => rhs.diff(lhs, unit)
-const addDiffToRemainder = (lhs, rhs, unit, diff) => lhs.add(moment.duration(diff, unit))
+const addDiffToRemainder = (lhs, rhs, unit, diff) => lhs.add(diff, unit)
 
-function getTimeBetweenDates (lhs, rhs = moment(), diffArr = []) {
-  diffArr = units.map((unit, index) => {
+function getTimeBetweenDates (lhs, rhs) {
+  const diffArr = units.map((unit, index) => {
     const diff = getDiff(...arguments, unit)
     addDiffToRemainder(...arguments, unit, diff)
     const newUnit = getUnit(diff, unit, index)
-    if (diff) { return { [newUnit]: diff } }
+    if (diff) { return { [newUnit]: Math.abs(diff) } }
   }).filter((unit, index) => unit)
-  console.log(diffArr)
   return diffArr
 }
 
 export class DateDiff {
-  constructor (lhs, rhs = moment()) {
+  constructor (lhs, rhs = moment(new Date())) {
     this.lhs = moment(lhs)
     this.rhs = rhs
     this.date = getTimeBetweenDates(this.lhs, this.rhs)
