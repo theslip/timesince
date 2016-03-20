4699491cb887da8625b8e6c629fbbd64a0df889c
David Rutsch <daverutsch@gmail.com>
2016-03-20 03:08:18 -0500

    Added logic to load bundle from correct url

--- src/views/HtmlWrapper.js
+++ src/views/HtmlWrapper.js
@@ -1,7 +1,11 @@
+import webconfig from '../../webconfig'
 import React, { PropTypes, Component } from 'react'
 import Helmet from 'react-helmet'
 import serialize from 'serialize-javascript'
 import { renderToString } from 'react-dom/server'
+
+const endpoints = {...webconfig.endpoints}
+const bundleHref = webconfig.isProd ? endpoints.timesince : endpoints.webpack
 
 export default class HtmlWrapper extends Component {
   static propTypes = {
@@ -19,12 +23,12 @@ export default class HtmlWrapper extends Component {
         <head>
           <title>{title}</title>
           <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' />
-          <link rel='stylesheet' type='text/css' href='/public/styles/site.css' />
+          <link rel='stylesheet' type='text/css' href={bundleHref + endpoints.styles + webconfig.cssName} />
           <link rel='icon' href='/favicon/favicon.ico' />
         </head>
         <body>
           <div id='content' dangerouslySetInnerHTML={{__html: content}}></div>
-          <script type='text/javascript' src='/public/scripts/bundle.js'></script>
+          <script type='text/javascript' src={bundleHref + webconfig.bundleName} charSet='UTF-8'></script>
           <script dangerouslySetInnerHTML={{__html: `window.__initialState__=${serialize(store.getState())};`}} charSet='UTF-8'/>
         </body>
       </html>
